93564d8d27b74d5ddf69f95cb27cd6ef7d17d251
Aladine <alazaiane@yahoo.fr>
2014-04-27 15:59:13 +0200

    suppresion iframe

--- app/assets/javascripts/sevran_futsal.js
+++ app/assets/javascripts/sevran_futsal.js
@@ -1,17 +1,6 @@
 $('document').ready(function() {
-	$('.samba-pic').click(function() {
-		$('.Didier').show('slow')
-	});
-
-	// $('.histoire').click(function() {
-	// 	$('iframe').show('slow')
-	// });
 
 	$('.histoire').click(function() {
 		$('.msg-histoire').show('slow')
 	});
-
-	// $('.samba-pro').click(function() {
-	// 	$('').hide('slow')
-	// });
 });
