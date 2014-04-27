55e8602ee990f2c9769f01db607e4dd51b7db266
Aladine <alazaiane@yahoo.fr>
2014-04-27 15:30:34 +0200

    message de service

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SevranFutsal::Application.routes.draw do
   get "home/index"
   get "joueurs/index"
+  get "videos/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
